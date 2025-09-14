2a7764ec9e2ced24a43c058302f72482614f11b5
reshma-hegde <reshmahegde853@gmail.com>
2025-09-14 16:35:21 +0530

    exception handling--partial implementation

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from ctypes import CFUNCTYPE, c_int, c_float
 
 LEXER_DEBUG: bool = False
 PARSER_DEBUG: bool = True
-COMPILER_DEBUG: bool =True
+COMPILER_DEBUG: bool =True 
 RUN_CODE:bool=True
 #try,qubitrestore,quantumtime,pause
 if __name__ == '__main__':
